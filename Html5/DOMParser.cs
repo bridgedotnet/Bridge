8624fc2e9c2a9f719af7cf39d8d1bcb3cd03353e
TriEdgeAI <triedgeai@gmail.com>
2016-09-03 00:25:13 +0300

    Oops 2.

--- Html5/DOMParser.cs
+++ Html5/DOMParser.cs
@@ -2,7 +2,7 @@
 {
     [External]
     [Name("DOMParser")]
-    class DOMParser
+    public class DOMParser
     {
         public extern DocumentInstance ParseFromString(string str, string mimeType);
     }
