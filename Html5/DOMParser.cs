6ab317c5151195042c613241f1055f2f32084c10
TriEdgeAI <triedgeai@gmail.com>
2016-09-02 23:48:02 +0300

    #1728: Support for DOMParser.

---
+++ Html5/DOMParser.cs
@@ -0,0 +1,9 @@
+﻿namespace Bridge.Html5
+{
+    [External]
+    [Name("DOMParser")]
+    class DOMParser
+    {
+        public extern DocumentInstance ParseFromString(string str, string mimeType);
+    }
+}
