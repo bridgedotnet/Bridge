a4c6500c3123fd32beb1c9cd681507bdbd780ae2
LeonidVeriga <leonid@object.net>
2016-09-02 19:48:18 +0300

    Merge Compiler tests to improve performance

--- Compiler/TranslatorTests/TestProjects/15/CheckProjectConstants.cs
+++ Compiler/TranslatorTests/TestProjects/15/CheckProjectConstants.cs
@@ -17,4 +17,14 @@
             return string.Empty;
         }
     }
+
+#if (TEST1)
+    public class ExistsIfTest1Defined { }
+#endif
+#if (TEST2)
+    public class ExistsIfTest2Defined { }
+#endif
+#if (TEST3)
+    public class NotExistsTest3{ }
+#endif
 }
