ea734087a0450cd1c1743142408a19122a138e2f
LeonidVeriga <leonid@object.net>
2016-09-19 17:58:20 +0300

    [1845] Add Client tests to check method, property and local var for `ctor` and `Ctor` names

--- Tests/Batch3/BridgeIssues/1800/N1845.cs
+++ Tests/Batch3/BridgeIssues/1800/N1845.cs
@@ -9,24 +9,65 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
     [TestFixture(TestNameFormat = "#1845 - {0}")]
     public class Bridge1845
     {
-        public class Base
+        public class Base1
         {
-            public Base(object target)
+            public Base1(object target)
             {
-                //Name = "ctor"
                 ctor();
+                Ctor();
             }
 
-            private void ctor()
+            public int ctor()
             {
-                //Name = "ctor" , Expected: not "ctor"
+                return 1;
+            }
+
+            public int Ctor()
+            {
+                return 2;
+            }
+        }
+
+        public class Base2
+        {
+            public Base2(object target)
+            {
+                var r1 = ctor;
+                var r2 = Ctor;
+            }
+
+            public int ctor
+            {
+                get
+                {
+                    return 1;
+                }
+            }
+
+            public int Ctor
+            {
+                get
+                {
+                    return 2;
+                }
             }
         }
 
         [Test]
-        public void TestCase()
+        public void TestCtorMemberName()
         {
-            Assert.NotNull(new Base(null));
+            var b1 = new Base1(null);
+            Assert.NotNull(b1, "b1");
+            Assert.AreEqual(1, b1.ctor(), "b1.ctor()");
+            Assert.AreEqual(2, b1.Ctor(), "b1.Ctor()");
+
+            var b2 = new Base2(null);
+            Assert.NotNull(b2, "b2");
+            Assert.AreEqual(1, b2.ctor, "b2.ctor");
+            Assert.AreEqual(2, b2.Ctor, "b2.Ctor");
+
+            var ctor = 3;
+            Assert.AreEqual(3, ctor, "var ctor");
         }
     }
 }
--- Tests/Runner/Batch3/code.js
+++ Tests/Runner/Batch3/code.js
@@ -6913,6 +6913,8 @@ Bridge.assembly("Bridge.ClientTest.Batch3", function ($asm, globals) {
                                 done = Bridge.Test.Assert.async();
 
                                 foo = null; /// Async method lacks 'await' operators and will run synchronously
+
+
                                 bar = function () {
                                     var $step = 0,
                                         $jumpFromFinally, 
@@ -6942,7 +6944,7 @@ Bridge.assembly("Bridge.ClientTest.Batch3", function ($asm, globals) {
 
                                     $asyncBody();
                                     return $tcs.task;
-                                }; /// Async method lacks 'await' operators and will run synchronously
+                                };
                                 $task1 = bar();
                                 $step = 1;
                                 $task1.continueWith($asyncBody, true);
@@ -9146,19 +9148,47 @@ Bridge.assembly("Bridge.ClientTest.Batch3", function ($asm, globals) {
     });
 
     Bridge.define("Bridge.ClientTest.Batch3.BridgeIssues.Bridge1845", {
-        testCase: function () {
-            Bridge.Test.Assert.notNull(new Bridge.ClientTest.Batch3.BridgeIssues.Bridge1845.Base(null));
+        testCtorMemberName: function () {
+            var b1 = new Bridge.ClientTest.Batch3.BridgeIssues.Bridge1845.Base1(null);
+            Bridge.Test.Assert.notNull$1(b1, "b1");
+            Bridge.Test.Assert.areEqual$1(1, b1.ctor$1(), "b1.ctor()");
+            Bridge.Test.Assert.areEqual$1(2, b1.ctor$2(), "b1.Ctor()");
+
+            var b2 = new Bridge.ClientTest.Batch3.BridgeIssues.Bridge1845.Base2(null);
+            Bridge.Test.Assert.notNull$1(b2, "b2");
+            Bridge.Test.Assert.areEqual$1(1, b2.getctor(), "b2.ctor");
+            Bridge.Test.Assert.areEqual$1(2, b2.getCtor(), "b2.Ctor");
+
+            var $ctor = 3;
+            Bridge.Test.Assert.areEqual$1(3, $ctor, "var ctor");
         }
     });
 
-    Bridge.define("Bridge.ClientTest.Batch3.BridgeIssues.Bridge1845.Base", {
+    Bridge.define("Bridge.ClientTest.Batch3.BridgeIssues.Bridge1845.Base1", {
         ctor: function (target) {
             this.$initialize();
-            //Name = "ctor"
             this.ctor$1();
+            this.ctor$2();
         },
         ctor$1: function () {
-            //Name = "ctor" , Expected: not "ctor"
+            return 1;
+        },
+        ctor$2: function () {
+            return 2;
+        }
+    });
+
+    Bridge.define("Bridge.ClientTest.Batch3.BridgeIssues.Bridge1845.Base2", {
+        ctor: function (target) {
+            this.$initialize();
+            var r1 = this.getctor();
+            var r2 = this.getCtor();
+        },
+        getctor: function () {
+            return 1;
+        },
+        getCtor: function () {
+            return 2;
         }
     });
 
