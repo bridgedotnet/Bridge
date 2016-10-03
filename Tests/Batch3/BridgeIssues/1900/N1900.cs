2cc10a1784f3ae41c272462f606f301df1d0ce4a
LeonidVeriga <leonid@object.net>
2016-10-04 02:42:42 +0300

    [1900] Add client tests to check to `out` parameters and one `ref`/one `out`

--- Tests/Batch3/BridgeIssues/1900/N1900.cs
+++ Tests/Batch3/BridgeIssues/1900/N1900.cs
@@ -7,8 +7,23 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
     [TestFixture(TestNameFormat = "#1900 - {0}")]
     public class Bridge1900
     {
-        [Bridge.Reflectable(true)]
-        private static bool TryGetValue(out int value)
+        [Reflectable(true)]
+        private static bool TryGetValue1(out int value)
+        {
+            value = 1;
+            return true;
+        }
+
+        [Reflectable(true)]
+        private static bool TryGetValue2(out int value, out string value2)
+        {
+            value = 1;
+            value2 = "";
+            return true;
+        }
+
+        [Reflectable(true)]
+        private static bool TestOutRef(out int value, ref string s)
         {
             value = 1;
             return true;
@@ -17,7 +32,9 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
         [Test]
         public void TestOutParamInMetadata()
         {
-            Assert.True(typeof(Bridge1900).GetMethod("TryGetValue").ParameterTypes[0] == typeof(int));
+            Assert.True(typeof(Bridge1900).GetMethod("TryGetValue1").ParameterTypes[0] == typeof(int));
+            Assert.True(typeof(Bridge1900).GetMethod("TryGetValue2").ParameterTypes[1] == typeof(string));
+            Assert.True(typeof(Bridge1900).GetMethod("TestOutRef").ParameterTypes[1] == typeof(string));
         }
     }
 }
