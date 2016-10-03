08bfb45fe394d0364ad40c868803461c31db6deb
vladsch <vladimir@object.net>
2016-10-04 00:54:18 +0300

    [1900] Syntax error in generated code of out parameter's reflection metadata

---
+++ Tests/Batch3/BridgeIssues/1900/N1900.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Bridge.Test;
+
+namespace Bridge.ClientTest.Batch3.BridgeIssues
+{
+    [Category(Constants.MODULE_ISSUES)]
+    [TestFixture(TestNameFormat = "#1900 - {0}")]
+    public class Bridge1900
+    {
+        [Bridge.Reflectable(true)]
+        private static bool TryGetValue(out int value)
+        {
+            value = 1;
+            return true;
+        }
+
+        [Test]
+        public void TestOutParamInMetadata()
+        {
+            Assert.True(typeof(Bridge1900).GetMethod("TryGetValue").ParameterTypes[0] == typeof(int));
+        }
+    }
+}
