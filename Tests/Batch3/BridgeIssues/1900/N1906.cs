abbeb4674586b7fa4c998a2be49b2579d58dec18
vladsch <vladimir@object.net>
2016-10-05 22:06:22 +0300

    [1906] Is operator inaccuracy

---
+++ Tests/Batch3/BridgeIssues/1900/N1906.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Bridge.Test;
+
+namespace Bridge.ClientTest.Batch3.BridgeIssues
+{
+    [Category(Constants.MODULE_ISSUES)]
+    [TestFixture(TestNameFormat = "#1906 - {0}")]
+    public class Bridge1906
+    {
+        [Test]
+        public void TestCase()
+        {
+#pragma warning disable 183
+            Assert.True(true is object);
+            Assert.True("string" is object);
+#pragma warning restore 183
+        }
+    }
+}
--- Tests/Runner/Batch3/code.js
+++ Tests/Runner/Batch3/code.js
@@ -6847,8 +6847,6 @@ Bridge.assembly("Bridge.ClientTest.Batch3", function ($asm, globals) {
                                 done = Bridge.Test.Assert.async();
 
                                 foo = null; /// Async method lacks 'await' operators and will run synchronously
-
-
                                 bar = function () {
                                     var $step = 0,
                                         $jumpFromFinally, 
@@ -6878,7 +6876,7 @@ Bridge.assembly("Bridge.ClientTest.Batch3", function ($asm, globals) {
 
                                     $asyncBody();
                                     return $tcs.task;
-                                };
+                                }; /// Async method lacks 'await' operators and will run synchronously
                                 $task1 = bar();
                                 $step = 1;
                                 $task1.continueWith($asyncBody, true);
@@ -10063,6 +10061,13 @@ Bridge.assembly("Bridge.ClientTest.Batch3", function ($asm, globals) {
             var d2 = new Date(tickValue.toNumber()/10000);
 
             Bridge.Test.Assert.true$1(Bridge.equals(d1, d2), System.String.concat(System.String.concat(System.String.concat(System.String.concat("d1 (", Bridge.Date.format(d1)), ") == d2("), Bridge.Date.format(d2)), ")"));
+        }
+    });
+
+    Bridge.define("Bridge.ClientTest.Batch3.BridgeIssues.Bridge1906", {
+        testCase: function () {
+            Bridge.Test.Assert.true(Bridge.is(true, Object));
+            Bridge.Test.Assert.true(Bridge.is("string", Object));
         }
     });
 
