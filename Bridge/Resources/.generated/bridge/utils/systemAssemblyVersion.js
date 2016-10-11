bc3371b00f45b133e799d5da28abd88332a07a05
LeonidVeriga <leonid@object.net>
2016-10-12 01:10:47 +0300

    [1193] Reflection.js to use `Bridge.assembly` parameter changes; Reflection.js split into three parts: assembly, autogenerated version and reflection code itself; `AssemblyVersionMarker` class to to get version string representation either of the current assembly or Compiler; `SystemAssemblyVersion` to get versions inside bridge.js

---
+++ Bridge/Resources/.generated/bridge/utils/systemAssemblyVersion.js
@@ -0,0 +1,6 @@
+﻿    (function(){
+        Bridge.SystemAssembly.version = "15.2.0";
+        Bridge.SystemAssembly.compiler = "15.2.0";
+    })();
+
+    Bridge.define("Bridge.Utils.SystemAssemblyVersion");
