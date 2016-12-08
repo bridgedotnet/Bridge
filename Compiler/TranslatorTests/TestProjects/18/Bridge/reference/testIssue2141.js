e2a6f65910fa8a9f9cfd8be01016733d8dff3140
LeonidVeriga <leonid@object.net>
2016-12-08 16:10:46 +0300

    [2141] Update Compiler test 18

--- Compiler/TranslatorTests/TestProjects/18/Bridge/reference/testIssue2141.js
+++ Compiler/TranslatorTests/TestProjects/18/Bridge/reference/testIssue2141.js
@@ -2,19 +2,67 @@
         statics: {
             test: function () {
                 // #2141 Skip writing type information to [External] [ObjectLiteral]
+
                 // These below should NOT contain Bridge.literal call
-                var config1 = { name: "external1" };
+                var c1 = { id: "1" };
+                // These below should contain Bridge.literal call as [Name] used
+                var c1Name = Bridge.literal(Options1, { id: "1Name" });
 
-                var config2 = { id: 2, name: "external2" };
+                // These below should NOT contain Bridge.literal call
+                // .ctor({ id: "..." });
+                var c2 = TestIssue2141.Config2.ctor({ id: "2" });
+                var c2Name = Options2.ctor({ id: "2Name" });
 
-                // These below should contain Bridge.literal call
-                var config3 = TestIssue2141.ConfigPlain.ctor({ name: "plain3" });
+                // These below should NOT contain Bridge.literal call
+                // Bridge.merge(.ctor(), { id: "..." });
+                var c3 = Bridge.merge(TestIssue2141.Config3.ctor(), {
+                    id: "3"
+                } );
+                var c3Name = Bridge.merge(Options3.ctor(), {
+                    id: "3Name"
+                } );
 
-                var config4 = TestIssue2141.ConfigPlain.ctor({ id: 4, name: "plain4" });
+                // These below should NOT contain Bridge.literal call
+                // Bridge.merge(Object(), { id: "4" });
+                var c4 = Bridge.merge(Object(), {
+                    id: "4"
+                } );
+                // Bridge.merge(Options4(), { id: "4Name" });
+                var c4Name = Bridge.merge(Options4(), {
+                    id: "4Name"
+                } );
             }
         }
     });
 
-    Bridge.define("TestIssue2141.ConfigPlain", {
+    Bridge.define("TestIssue2141.Config2", {
         $literal: true
+    });
+
+    Bridge.define("Options2", {
+        $literal: true
+    });
+
+    Bridge.define("TestIssue2141.Config3", {
+        $literal: true,
+        ctor: function () {
+            var $this = {};
+            $this.$getType = function() { return TestIssue2141.Config3; };
+            (function(){
+                this.id = null;
+            }).call($this);
+            return $this;
+        }
+    });
+
+    Bridge.define("Options3", {
+        $literal: true,
+        ctor: function () {
+            var $this = {};
+            $this.$getType = function() { return Options3; };
+            (function(){
+                this.id = null;
+            }).call($this);
+            return $this;
+        }
     });
--- Compiler/TranslatorTests/TestProjects/18/Issues/Issue2141.cs
+++ Compiler/TranslatorTests/TestProjects/18/Issues/Issue2141.cs
@@ -1,32 +1,83 @@
 ﻿using Bridge;
 
-//#1035
+//#2141
 namespace TestIssue2141
 {
     [External]
     [ObjectLiteral]
-    public class ConfigExternal
+    public class Config1
     {
-        public int Id
+        public string Id
         {
             get; set;
         }
+    }
 
-        public string Name
+    [Name("Options1")]
+    [External]
+    [ObjectLiteral]
+    public class Config1Name
+    {
+        public string Id
         {
             get; set;
         }
     }
 
     [ObjectLiteral]
-    public class ConfigPlain
+    public class Config2
     {
-        public int Id
+        public string Id
         {
             get; set;
         }
+    }
 
-        public string Name
+    [Name("Options2")]
+    [ObjectLiteral]
+    public class Config2Name
+    {
+        public string Id
+        {
+            get; set;
+        }
+    }
+
+    [ObjectLiteral(ObjectCreateMode.Constructor)]
+    public class Config3
+    {
+        public string Id
+        {
+            get; set;
+        }
+    }
+
+    [Name("Options3")]
+    [ObjectLiteral(ObjectCreateMode.Constructor)]
+    public class Config3Name
+    {
+        public string Id
+        {
+            get; set;
+        }
+    }
+
+    [External]
+    [ObjectLiteral(ObjectCreateMode.Constructor)]
+    public class Config4
+    {
+        public string Id
+        {
+            get; set;
+        }
+    }
+
+    [Name("Options4")]
+    [External]
+    [ObjectLiteral(ObjectCreateMode.Constructor)]
+    public class Config4Name
+    {
+        public string Id
         {
             get; set;
         }
@@ -37,29 +88,27 @@ namespace TestIssue2141
         public static void Test()
         {
             // #2141 Skip writing type information to [External] [ObjectLiteral]
+
             // These below should NOT contain Bridge.literal call
-            var config1 = new ConfigExternal
-            {
-                Name = "external1"
-            };
+            var c1 = new Config1() { Id = "1" };
+            // These below should contain Bridge.literal call as [Name] used
+            var c1Name = new Config1Name() { Id = "1Name" };
 
-            var config2 = new ConfigExternal
-            {
-                Id = 2,
-                Name = "external2"
-            };
+            // These below should NOT contain Bridge.literal call
+            // .ctor({ id: "..." });
+            var c2 = new Config2() { Id = "2" };
+            var c2Name = new Config2Name() { Id = "2Name" };
 
-            // These below should contain Bridge.literal call
-            var config3 = new ConfigPlain
-            {
-                Name = "plain3"
-            };
+            // These below should NOT contain Bridge.literal call
+            // Bridge.merge(.ctor(), { id: "..." });
+            var c3 = new Config3() { Id = "3" };
+            var c3Name = new Config3Name() { Id = "3Name" };
 
-            var config4 = new ConfigPlain
-            {
-                Id = 4,
-                Name = "plain4"
-            };
+            // These below should NOT contain Bridge.literal call
+            // Bridge.merge(Object(), { id: "4" });
+            var c4 = new Config4() { Id = "4" };
+            // Bridge.merge(Options4(), { id: "4Name" });
+            var c4Name = new Config4Name() { Id = "4Name" };
         }
     }
 }
