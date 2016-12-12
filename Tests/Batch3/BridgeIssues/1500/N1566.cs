6932b5d83730e2460137669b6b9108bdecbc5dcf
LeonidVeriga <leonid@object.net>
2016-12-12 14:40:29 +0300

    [1566] Log tests to use `AssertAlmostEqual`

--- Tests/Batch3/BridgeIssues/1500/N1566.cs
+++ Tests/Batch3/BridgeIssues/1500/N1566.cs
@@ -13,35 +13,46 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
         [Test]
         public void TestMathLog10()
         {
-            Assert.AreEqual(0.477121254719662, Math.Log10(3.0));
-            Assert.AreEqual(double.NegativeInfinity, Math.Log10(0.0));
-            Assert.AreEqual(double.NaN, Math.Log10(-3.0));
-            Assert.AreEqual(double.NaN, Math.Log10(double.NaN));
-            Assert.AreEqual(double.PositiveInfinity, Math.Log10(double.PositiveInfinity));
-            Assert.AreEqual(double.NaN, Math.Log10(double.NegativeInfinity));
+            AssertAlmostEqual(0.477121254719662, Math.Log10(3.0));
+            AssertAlmostEqual(double.NegativeInfinity, Math.Log10(0.0));
+            AssertAlmostEqual(double.NaN, Math.Log10(-3.0));
+            AssertAlmostEqual(double.NaN, Math.Log10(double.NaN));
+            AssertAlmostEqual(double.PositiveInfinity, Math.Log10(double.PositiveInfinity));
+            AssertAlmostEqual(double.NaN, Math.Log10(double.NegativeInfinity));
         }
 
         [Test]
         public void TestMathLogWithBase()
         {
-            Assert.AreEqual(1.0, Math.Log(3.0, 3.0));
-            Assert.AreEqual(2.40217350273, Math.Log(14, 3.0));
-            Assert.AreEqual(double.NegativeInfinity, Math.Log(0.0, 3.0));
-            Assert.AreEqual(double.NaN, Math.Log(-3.0, 3.0));
-            Assert.AreEqual(double.NaN, Math.Log(double.NaN, 3.0));
-            Assert.AreEqual(double.PositiveInfinity, Math.Log(double.PositiveInfinity, 3.0));
-            Assert.AreEqual(double.NaN, Math.Log(double.NegativeInfinity, 3.0));
+            AssertAlmostEqual(1.0, Math.Log(3.0, 3.0));
+            AssertAlmostEqual(2.40217350273, Math.Log(14, 3.0));
+            AssertAlmostEqual(double.NegativeInfinity, Math.Log(0.0, 3.0));
+            AssertAlmostEqual(double.NaN, Math.Log(-3.0, 3.0));
+            AssertAlmostEqual(double.NaN, Math.Log(double.NaN, 3.0));
+            AssertAlmostEqual(double.PositiveInfinity, Math.Log(double.PositiveInfinity, 3.0));
+            AssertAlmostEqual(double.NaN, Math.Log(double.NegativeInfinity, 3.0));
         }
 
         [Test]
         public void TestMathLog()
         {
-            Assert.AreEqual(1.09861228866811, Math.Log(3.0));
-            Assert.AreEqual(double.NegativeInfinity, Math.Log(0.0));
-            Assert.AreEqual(double.NaN, Math.Log(-3.0));
-            Assert.AreEqual(double.NaN, Math.Log(double.NaN));
-            Assert.AreEqual(double.PositiveInfinity, Math.Log(double.PositiveInfinity));
-            Assert.AreEqual(double.NaN, Math.Log(double.NegativeInfinity));
+            AssertAlmostEqual(1.09861228866811, Math.Log(3.0));
+            AssertAlmostEqual(double.NegativeInfinity, Math.Log(0.0));
+            AssertAlmostEqual(double.NaN, Math.Log(-3.0));
+            AssertAlmostEqual(double.NaN, Math.Log(double.NaN));
+            AssertAlmostEqual(double.PositiveInfinity, Math.Log(double.PositiveInfinity));
+            AssertAlmostEqual(double.NaN, Math.Log(double.NegativeInfinity));
+        }
+
+        private void AssertAlmostEqual(double expected, double actual)
+        {
+            var diff = actual - expected;
+            if (diff < 0)
+            {
+                diff = -diff;
+            }
+
+            Assert.True(diff < 1e-8, "Actual:" + actual + " vs Expected:" + expected);
         }
     }
 }
