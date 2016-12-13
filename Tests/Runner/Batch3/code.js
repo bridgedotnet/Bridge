4cb0362444c61c2c6ce2f8e30f46ba0089d4a458
vladsch <vladimir@object.net>
2016-12-13 15:47:51 +0300

    [2163] System.Decimal.ToFormat fails

--- Tests/Runner/Batch3/code.js
+++ Tests/Runner/Batch3/code.js
@@ -7166,8 +7166,6 @@ Bridge.$N1391Result =                 r;
                                 done = Bridge.Test.Assert.async();
 
                                     foo = null; /// Async method lacks 'await' operators and will run synchronously
-
-
                                     bar = Bridge.fn.bind(this, function () {
                                         var $step = 0,
                                             $jumpFromFinally, 
@@ -7197,7 +7195,7 @@ Bridge.$N1391Result =                 r;
 
                                         $asyncBody();
                                         return $tcs.task;
-                                    });
+                                    }); /// Async method lacks 'await' operators and will run synchronously
                                     $task1 = bar();
                                     $step = 1;
                                     $task1.continueWith($asyncBody, true);
@@ -12486,6 +12484,16 @@ Bridge.$N1391Result =                 r;
             this.setTestIntProperty(1);
             this.setTestStringProperty("constructor");
             this.setTestObjectProperty(String.fromCharCode.apply(null, [99]));
+        }
+    });
+
+    Bridge.define("Bridge.ClientTest.Batch3.BridgeIssues.Bridge2163", {
+        statics: {
+            testDecimalToFormat: function () {
+                var d1 = System.Decimal(1.0);
+                Bridge.Test.Assert.areEqual("1.00", d1.toFormat(2));
+                Bridge.Test.Assert.areEqual("1", d1.toFormat());
+            }
         }
     });
 
