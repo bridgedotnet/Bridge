c5b362e49ec4bed5787be071d77a8aeab789ce4a
vladsch <vladimir@object.net>
2016-12-15 22:48:19 +0300

    [2167] Bridge.merge wrong behaviour for primitive class members

---
+++ Tests/Batch3/BridgeIssues/2100/N2167.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Bridge.Test;
+
+namespace Bridge.ClientTest.Batch3.BridgeIssues
+{
+    [Category(Constants.MODULE_ISSUES)]
+    [TestFixture(TestNameFormat = "#2167 - {0}")]
+    public class Bridge2167
+    {
+        public class c1
+        {
+            public decimal p1 { get; set; }
+            public decimal p2 { get; set; }
+        }
+
+        [Test]
+        public static void TestMerge()
+        {
+            object o1 = new
+            {
+                p1 = 1
+            };
+
+            c1 o2 = new c1()
+            {
+                p1 = 2.0m,
+                p2 = 2.0m
+            };
+            o2 = Script.Write<c1>("Bridge.merge(o2, o1);");
+
+            decimal o3 = 3.0m;
+#pragma warning disable 219
+            object o4 = 1;
+#pragma warning restore 219
+            o3 = Script.Write<decimal>("Bridge.merge(o3, o4);");
+
+            Assert.True(o2.p1.Is<decimal>());
+            Assert.True(o2.p2.Is<decimal>());
+            Assert.True(o3.Is<decimal>());
+        }
+    }
+}
