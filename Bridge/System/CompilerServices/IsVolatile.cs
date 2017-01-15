e0d41f37b4e3835317b64e4880f37aba3071c766
LeonidVeriga <leonid@object.net>
2017-01-16 00:43:11 +0300

    [2225] Amend comments

--- Bridge/System/CompilerServices/IsVolatile.cs
+++ Bridge/System/CompilerServices/IsVolatile.cs
@@ -3,10 +3,12 @@ using System.ComponentModel;
 
 namespace System.Runtime.CompilerServices
 {
+    /// <summary>
+    /// Should compile, but be ignored as volatile is not relevant in the JavaScript world.
+    /// </summary>
     [System.Runtime.InteropServices.ComVisible(true)]
     [External]
     public static class IsVolatile
     {
-        // no instantiation, please!
     }
 }
