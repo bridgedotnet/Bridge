b666419db15f0c4973ae5e807cfa50f08e56ce90
LeonidVeriga <leonid@object.net>
2017-01-31 21:41:24 +0300

    [2298] Add client test

--- Tests/Batch3/BridgeIssues/2200/N2298.cs
+++ Tests/Batch3/BridgeIssues/2200/N2298.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Bridge.Test;
+using Bridge.Test.NUnit;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
@@ -37,13 +37,51 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             }
         }
 
+        public class DecimalBugTest<TX>
+        {
+            public IThing<V2<TX, decimal>, V2<decimal, long>> _MyThing;
+
+            public string DoStuff()
+            {
+                return _MyThing.MyMethod();
+            }
+        }
+
+        public class DecimalThing<TX> : IThing<V2<TX, decimal>, V2<decimal, long>>
+        {
+            public V2<TX, decimal> A
+            {
+                get; set;
+            }
+
+            public V2<decimal, long> B
+            {
+                get; set;
+            }
+
+            public string MyMethod()
+            {
+                return string.Join("|", new [] { A.a.ToString(), A.b.ToString(), B.a.ToString(), B.b.ToString()});
+            }
+        }
+
         [Test]
-        public static void TestGenericInetrfaceWithNestedTypeParameters()
+        public static void TestGenericInterfaceWithNestedTypeParameters()
         {
             var b = new BugTest<string>();
             b._MyThing = new MyThing<string>();
 
             Assert.AreEqual("MyThing", b.DoStuff());
+
+
+            var m = new DecimalBugTest<decimal>();
+            m._MyThing = new DecimalThing<decimal>()
+            {
+                A = new V2<decimal, decimal>() { a = 1.1m, b = 2.2m },
+                B = new V2<decimal, long>() { a = 3.3m, b = 4L }
+            };
+
+            Assert.AreEqual("1.1|2.2|3.3|4", m.DoStuff());
         }
     }
 }
