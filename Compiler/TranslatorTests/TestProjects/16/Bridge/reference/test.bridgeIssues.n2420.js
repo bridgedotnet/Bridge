3a5d5f19e2ff4901f2b5331fac69f9f7c1a074bf
LeonidVeriga <leonid@object.net>
2017-03-03 17:51:06 +0300

    [2420] Adjust IntegrationTest (external enum - no boxing)

--- Compiler/TranslatorTests/TestProjects/16/Bridge/reference/test.bridgeIssues.n2420.js
+++ Compiler/TranslatorTests/TestProjects/16/Bridge/reference/test.bridgeIssues.n2420.js
@@ -4,18 +4,25 @@ Bridge.assembly("TestProject", function ($asm, globals) {
     Bridge.define("Test.BridgeIssues.N2420.N2420", {
         statics: {
             output: function () {
+                var externalOption = "one";
+
+                // Bridge boxes external enum value for Console.WriteLine(Enum value)
+                // but it is not required because Template handles it. It produces unnecessary code.
+                // Expected Bridge.Console.log(System.Enum.toString(...Options.format, externalOption));
+                Bridge.Console.log(System.Enum.toString(System.String, externalOption));
+
                 var option = "one";
 
-                // Bridge box enum value for Console.WriteLine(Enum value) but it is not required because Template handles it. It produces unnecessary code.
-                // Expected Bridge.Console.log(System.Enum.toString(...Options.format, option));
-                Bridge.Console.log(System.Enum.toString(Test.BridgeIssues.N2420.Options.format, Bridge.box(option, Test.BridgeIssues.N2420.Options.format, $box_.Test.BridgeIssues.N2420.Options.format.toString)));
+                // Bridge boxes enum value for Console.WriteLine(Enum value)
+                // Expected Bridge.Console.log(...box);
+                Bridge.Console.log(System.Enum.toString(Test.BridgeIssues.N2420.Options.Format, Bridge.box(option, Test.BridgeIssues.N2420.Options.Format, $box_.Test.BridgeIssues.N2420.Options.Format.toString)));
             }
         }
     });
 
     Bridge.define("Test.BridgeIssues.N2420.Options");
 
-    Bridge.define("Test.BridgeIssues.N2420.Options.format", {
+    Bridge.define("Test.BridgeIssues.N2420.Options.Format", {
         $kind: "enum",
         statics: {
             one: "one",
--- Compiler/TranslatorTests/TestProjects/16/BridgeIssues/2000/N2420.cs
+++ Compiler/TranslatorTests/TestProjects/16/BridgeIssues/2000/N2420.cs
@@ -9,10 +9,17 @@ namespace Test.BridgeIssues.N2420
     {
         public static void Output()
         {
-            var option = Options.format.one;
+            var externalOption = Options.ExternalFormat.one;
 
-            // Bridge box enum value for Console.WriteLine(Enum value) but it is not required because Template handles it. It produces unnecessary code.
-            // Expected Bridge.Console.log(System.Enum.toString(...Options.format, option));
+            // Bridge boxes external enum value for Console.WriteLine(Enum value)
+            // but it is not required because Template handles it. It produces unnecessary code.
+            // Expected Bridge.Console.log(System.Enum.toString(...Options.format, externalOption));
+            Console.WriteLine(externalOption);
+
+            var option = Options.Format.one;
+
+            // Bridge boxes enum value for Console.WriteLine(Enum value)
+            // Expected Bridge.Console.log(...box);
             Console.WriteLine(option);
         }
     }
@@ -20,7 +27,15 @@ namespace Test.BridgeIssues.N2420
     public static class Options
     {
         [Enum(Emit.StringName)]
-        public enum format
+        public enum Format
+        {
+            one,
+            two
+        }
+
+        [External]
+        [Enum(Emit.StringName)]
+        public enum ExternalFormat
         {
             one,
             two
