aaf45dbb92e82a12e1c17941604b7426ab63205b
LeonidVeriga <leonid@object.net>
2017-03-22 22:11:49 +0300

    [2456] Add client test for Bridge.isArray() for an array from iframe

---
+++ Tests/Batch3/BridgeIssues/2400/N2456.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bridge.Test.NUnit;
+using Bridge.Html5;
+
+namespace Bridge.ClientTest.Batch3.BridgeIssues
+{
+    [Category(Constants.MODULE_ISSUES)]
+    [TestFixture(TestNameFormat = "#2456 - {0}")]
+    public class Bridge2456
+    {
+        [External]
+        [Name("Bridge")]
+        private class BridgeHelper
+        {
+            [Unbox(false)]
+            public static extern bool isArray(object o);
+        }
+
+        [Test]
+        public static void TestIsArrayFromIFrame()
+        {
+            var frame = new HTMLIFrameElement();
+            Document.Body.AppendChild(frame);
+
+            try
+            {
+                dynamic xFrame = Window.Frames[Window.Frames.Length - 1];
+                object xArray = xFrame.Array;
+
+                var array = Script.Write<object>("new {0}(1,2,3)", xArray);
+                var isArray = BridgeHelper.isArray(array);
+
+                Assert.True(isArray, "Checks that an array created in another frame returns true for Bridge.isArray(array)");
+            }
+            finally
+            {
+                if (frame != null && frame.ParentNode != null)
+                {
+                    frame.ParentNode.RemoveChild(frame);
+                }
+            }
+        }
+    }
+}
