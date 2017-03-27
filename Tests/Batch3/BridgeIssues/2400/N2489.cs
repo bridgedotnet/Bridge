5a6b15d9f8d8bad086b2670828e19bf98d198285
LeonidVeriga <leonid@object.net>
2017-03-27 14:03:01 +0300

    [2489] Correct client test

--- Tests/Batch3/BridgeIssues/2400/N2489.cs
+++ Tests/Batch3/BridgeIssues/2400/N2489.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Bridge.Test.NUnit;
@@ -9,31 +10,36 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
     [TestFixture(TestNameFormat = "#2489 - {0}")]
     public class Bridge2489
     {
-        [Reflectable(Inherits = false)]
-        public class Bar : BaseClass
-        {
-            public readonly int Field01 = 1;
-        }
-
-        public class Bar2 : Bar
-        {
-            public readonly int Field02 = 1;
-        }
-
         [Reflectable(Inherits = true)]
         public class BaseClass
         {
+            public readonly int Field = 1;
         }
 
-        [Template("Bridge.getMetadata({t})")]
-        public static extern object GetMetadata(Type t);
+        public class Bar : BaseClass
+        {
+            public readonly int Field00 = 2;
+        }
+
+        [Reflectable(Inherits = false)]
+        public class Bar1 : Bar
+        {
+            public readonly int Field01 = 3;
+        }
+
+        public class Bar2 : Bar1
+        {
+            public readonly int Field02 = 4;
+        }
 
         [Test]
         public static void TestReflectableInherits()
         {
-            Assert.NotNull(GetMetadata(typeof(BaseClass)));
-            Assert.NotNull(GetMetadata(typeof(Bar)));
-            Assert.Null(GetMetadata(typeof(Bar2)));
+            Assert.AreEqual(MemberTypes.Field, typeof(BaseClass).GetField("Field").MemberType, "Should have Field in metadata as type has its own [Reflectable]");
+            Assert.AreEqual(MemberTypes.Field, typeof(Bar).GetField("Field00").MemberType, "Should have Field in metadata as base type has [Reflectable] with Inherits = true");
+            Assert.AreEqual(MemberTypes.Field, typeof(Bar1).GetField("Field01").MemberType, "Should have Field in metadata as type has its own [Reflectable]");
+
+            Assert.AreEqual(null, typeof(Bar2).GetField("Field02"), "Should NOT have Field in metadata as base type has [Reflectable] with Inherits = false");
         }
     }
 }
