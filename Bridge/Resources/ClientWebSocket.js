75ac33e8a0d78a995b6f7cb0b71e33207ac28233
Dierk Droth <dierk.droth@ninjatrader.com>
2017-04-03 14:04:42 +0100

    Fix for this issue https://forums.bridge.net/forum/bridge-net-pro/bugs/3900-websocket-sendasync-not-implemented-correctly

--- Bridge/Resources/ClientWebSocket.js
+++ Bridge/Resources/ClientWebSocket.js
@@ -98,21 +98,21 @@
             var tcs = new System.Threading.Tasks.TaskCompletionSource();
 
             try {
-                var array = buffer.getArray(),
+                var array = buffer.getArray(), count = buffer.getCount(), offset = buffer.getOffset(),
                     data;
 
                 switch (messageType) {
                 case "binary":
-                    data = new ArrayBuffer(array.length);
+                    data = new ArrayBuffer(count);
                     var dataView = new Int8Array(data);
 
-                    for (var i = 0; i < array.length; i++) {
-                        dataView[i] = array[i];
+                    for (var i = 0; i < count; i++) {
+                        dataView[i] = array[i + offset];
                     }
 
                     break;
                 case "text":
-                    data = String.fromCharCode.apply(null, array);
+                    data = String.fromCharCode.apply(null, new Uint8Array(array, offset, count));
                     break;
                 }
 
