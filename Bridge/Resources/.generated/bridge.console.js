a14c0ae44e058f918ed0042842c57d84710a6fda
Geoffrey McGill <geoff@object.net>
2017-04-20 23:04:24 -0600

    Build with latest from master branch

--- Bridge/Resources/.generated/bridge.console.js
+++ Bridge/Resources/.generated/bridge.console.js
@@ -24,38 +24,30 @@
                 var con = Bridge.global.console;
 
                 if (wl) {
-                    
-                    System.Console.WriteLine = function (value) {
-                        wl(value);
-                        Bridge.Console.log(value);
-                    }
-                    
+                        System.Console.WriteLine = function (value) {
+                            wl(value);
+                            Bridge.Console.log(value);
+                        }
                 }
 
                 if (debug) {
-                    
-                    System.Diagnostics.Debug.writeln = function (value) {
-                        debug(value);
-                        Bridge.Console.debug(value);
-                    }
-                 
+                        System.Diagnostics.Debug.writeln = function (value) {
+                            debug(value);
+                            Bridge.Console.debug(value);
+                        }
                 }
 
                 if (win) {
-                    
-                    Bridge.global.window.addEventListener("error", function (e) {
-                        Bridge.Console.error(e.message + " (" + e.filename.substring(e.filename.lastIndexOf('/') + 1) + ", line " + e.lineno + " char " + e.colno + ")");
-                    });
-                 
+                        Bridge.global.window.addEventListener("error", function (e) {
+                            Bridge.Console.error(e.message + " (" + e.filename.substring(e.filename.lastIndexOf('/') + 1) + ", line " + e.lineno + " char " + e.colno + ")");
+                        });
                 }
 
                 if (con && con.error) {
-                    
-                    Bridge.global.console.error = function (msg) {
-                        error(msg);
-                        Bridge.Console.error(msg);
-                    }
-                 
+                        Bridge.global.console.error = function (msg) {
+                            error(msg);
+                            Bridge.Console.error(msg);
+                        }
                 }
             },
             logBase: function (value, messageType) {
@@ -65,9 +57,7 @@
                 var t = false;
 
                 if (value != null) {
-                    
-                    t = typeof value !== "object";
-                  
+                        t = typeof value !== "object";
 
                     if (Bridge.getType(value).isPrimitive || t) {
                         v = value == null ? "" : value.toString();
@@ -460,4 +450,4 @@
         }
     });
 
-    Bridge.init(function() { Bridge.Console.end(); });
+    Bridge.init(function () { Bridge.Console.end(); });
