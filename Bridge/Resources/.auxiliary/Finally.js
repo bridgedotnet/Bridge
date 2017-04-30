0c9d4a49ece0dfac763b9bbc5273c3223aef00fe
retsimx <lewis@retsim.com>
2017-04-30 13:07:52 +1000

    Added some missing comments

--- Bridge/Resources/.auxiliary/Finally.js
+++ Bridge/Resources/.auxiliary/Finally.js
@@ -3,5 +3,6 @@
 // WebWorker check
 // http://stackoverflow.com/a/18002694
 if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope) {
+	// Call the web worker manager entry point
     System.Threading.Utils.WorkerThreadManager.workerThreadManagerEntryPoint();
 }
