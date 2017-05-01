539d72c830e643745aa8870144c9c118f3b99855
retsimx <lewis@retsim.com>
2017-05-01 17:26:43 +1000

    Fixed merge conflicts

--- Bridge/Resources/.auxiliary/Finally.js
+++ Bridge/Resources/.auxiliary/Finally.js
@@ -4,9 +4,5 @@
 // http://stackoverflow.com/a/18002694
 if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope) {
 	// Call the web worker manager entry point
-<<<<<<< HEAD
     Bridge.init(System.Threading.Utils.WorkerThreadManager.workerThreadManagerEntryPoint);
-=======
-    System.Threading.Utils.WorkerThreadManager.workerThreadManagerEntryPoint();
->>>>>>> ab3209ca4eac8b376a91f6c5acf790d9d4e3b56a
 }
