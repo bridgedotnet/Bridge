907f9e850ff44b426793b0d78a726f43d78329e6
LeonidVeriga <leonid@object.net>
2017-05-13 19:44:35 +0300

    [2630] Update Integration test reference

--- Compiler/TranslatorTests/TestProjects/16/Bridge/reference/test.bridgeIssues.n2277.js
+++ Compiler/TranslatorTests/TestProjects/16/Bridge/reference/test.bridgeIssues.n2277.js
@@ -4,6 +4,7 @@ Bridge.assembly("TestProject", function ($asm, globals) {
     Bridge.define("Test.BridgeIssues.N2277.N2277", {
         methods: {
             DoSomething: function () {
+
             }
         }
     });
