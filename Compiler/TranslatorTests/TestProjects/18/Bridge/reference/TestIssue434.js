8fa8914178858f9b1c1abfc3acfacaf8961f2a4a
LeonidVeriga <leonid@object.net>
2017-06-05 22:35:05 +0300

    FileNameCasing: Fix Integration Tests

--- Compiler/TranslatorTests/TestProjects/18/Bridge/reference/TestIssue434.js
+++ Compiler/TranslatorTests/TestProjects/18/Bridge/reference/TestIssue434.js
@@ -1,8 +1,3 @@
-// Content header: 1.0.0-beta  {notdate} 2017 
-// ---
-// Content remark: Path: Bridge/output/TestIssue434.js; name: TestIssue434.js
-// ---
-
 // Top
 
     Bridge.init(function () {
@@ -62,47 +57,3 @@
     Bridge.define("TestIssue434.Issue434C");
 
 // Bottom
-// ---
-// Content remark: Path: Bridge/output/TestIssue461.js; name: TestIssue461.js
-// ---
-
-    Bridge.define("TestIssue461.Issue461", {
-        statics: {
-            methods: {
-                Test: function Test() {
-                    var input = document.createElement('input');
-
-                    input.onchange = Bridge.fn.combine(input.onchange, $asm.$.TestIssue461.Issue461.f1);
-
-                    var anchor = document.createElement('a');
-
-                    anchor.onclick = Bridge.fn.combine(anchor.onclick, $asm.$.TestIssue461.Issue461.f2);
-
-                    // Test if Document.GetElementById<>() compiles
-                    var div = document.getElementById("div1");
-
-                    // Tests if Element is still a superclass of all the element classes and the following code compiles
-                    var element;
-
-                    element = document.createElement('input');
-                    element = document.createElement('textarea');
-                }
-            }
-        }
-    });
-
-    Bridge.ns("TestIssue461.Issue461", $asm.$);
-
-    Bridge.apply($asm.$.TestIssue461.Issue461, {
-        f1: function (ev) {
-            // Tests if ev.CurrentTarget.Value compiles
-            System.Console.WriteLine(System.String.concat("ev.CurrentTarget.Value: ", ev.currentTarget.value));
-
-            // Tests if ev.IsMouseEvent() compiles
-            System.Console.WriteLine("IsMouseEvent: " + System.Boolean.toString(Bridge.is(ev, MouseEvent)));
-        },
-        f2: function (ev) {
-            // Tests if ev.CurrentTarget.Href compiles
-            System.Console.WriteLine(System.String.concat("ev.CurrentTarget.Href: ", ev.currentTarget.href));
-        }
-    });
