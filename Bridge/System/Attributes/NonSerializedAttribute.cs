e3397c7cc59ddc6d385e57a54156ed0891243c54
Michael Cheers <michaelcheers8@gmail.com>
2017-06-26 10:23:34 +0200

    Added NonSerializedAttribute.

---
+++ Bridge/System/Attributes/NonSerializedAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace System
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = false)]
+    [ComVisible(true)]
+    [External, NonScriptable]
+    public sealed class NonSerializedAttribute : Attribute
+    {
+
+    }
+}
