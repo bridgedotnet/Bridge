37f232723a834b97f9c511ed1f17d3c78c143fd2
cra0zy <cra0zy@gmail.com>
2017-09-27 03:09:04 +0200

    Add MonoDevelop addin

---
+++ .build/templates/MonoDevelop.Bridge.Addin/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "MonoDevelop.Bridge.Addin",
+    Version = "1.0"
+)]
+
+[assembly: AddinName("Bridge.NET Templates")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("Provides Bridge.NET project teplates.")]
+[assembly: AddinAuthor("cra0zy")]
+[assembly: AddinUrl("https://github.com/bridgedotnet/Bridge")]
