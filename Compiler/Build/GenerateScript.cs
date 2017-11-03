dd035d6ff92c5f6acbf9f1a5e0b8d8aaaa2fe9ea
Geoffrey McGill <geoff@object.net>
2017-11-02 21:58:18 -0600

    Test logger msg

--- Compiler/Build/GenerateScript.cs
+++ Compiler/Build/GenerateScript.cs
@@ -129,7 +129,8 @@ namespace Bridge.Build
 #endif
             var logger = new Translator.Logging.Logger(null, false, LoggerLevel.Info, true, new VSLoggerWriter(this.Log), new FileLoggerWriter());
 
-            logger.Error("Executing Bridge.Build.Task...");
+            logger.Trace("123 ABC");
+            logger.Trace("Executing Bridge.Build.Task...");
 
             var bridgeOptions = this.GetBridgeOptions();
 
@@ -245,6 +246,7 @@ namespace Bridge.Build
             }
 
             bool b;
+
             if (bool.TryParse(this.CheckForOverflowUnderflow, out b))
             {
                 return b;
