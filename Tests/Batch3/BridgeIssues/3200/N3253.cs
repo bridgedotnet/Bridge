0e0364830b084f1ddba643ab95d6eaf7b3666378
Fabrício Zimmerer Murta <fabricio@object.net>
2017-11-06 22:59:20 -0200

    Comments and expands unit test on issue 3253.
    The expanded scenarios are failing.

--- Tests/Batch3/BridgeIssues/3200/N3253.cs
+++ Tests/Batch3/BridgeIssues/3200/N3253.cs
@@ -1,31 +1,52 @@
-using System;
 using Bridge.Test.NUnit;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// This test consists in checking whether class properties' CanWrite()
+    /// method returns a value congruent to C# and the class definition.
+    /// </summary>
     [Category(Constants.MODULE_ISSUES)]
     [TestFixture(TestNameFormat = "#3253 - {0}")]
     public class Bridge3253
     {
+        /// <summary>
+        /// A subject class with a read-only and read-write properties
+        /// </summary>
         [Reflectable]
         public class Person
         {
             public Person(int id, string name)
             {
                 Id = id;
+                RWId = id;
                 Name = name;
+                RWName = name;
             }
             public int Id { get; }
             public string Name { get; }
+            public int RWId { get; }
+            public string RWName { get; }
         }
 
+        /// <summary>
+        /// Check each class' property whether they have the expected
+        /// CanWrite() state
+        /// </summary>
         [Test]
         public static void TestCanSetForReadonlyProperty()
         {
             var idProperty = typeof(Person).GetProperty("Id");
-            Assert.False(idProperty.CanWrite);
+            Assert.False(idProperty.CanWrite, "Readonly Id property has CanWrite() == false");
+
+            var nameProperty = typeof(Person).GetProperty("Name");
+            Assert.False(nameProperty.CanWrite, "Readonly Name property has CanWrite() == false");
+
+            var rwidProperty = typeof(Person).GetProperty("RWId");
+            Assert.True(idProperty.CanWrite, "Read-write RWId property has CanWrite() == true");
+
+            var rwnameProperty = typeof(Person).GetProperty("RWName");
+            Assert.True(idProperty.CanWrite, "Read-write RWName property has CanWrite() == true");
         }
     }
 }
--- Tests/Runner/Batch3/Bridge.Test/bridge.clienttest.batch3.runner.js
+++ Tests/Runner/Batch3/Bridge.Test/bridge.clienttest.batch3.runner.js
@@ -14313,7 +14313,7 @@ Bridge.assembly("Bridge.Test.Bridge.ClientTest.Batch3", function ($asm, globals)
             methods: {
                 TestCanSetForReadonlyProperty: function (assert) {
                     var $t;
-                    var t = Bridge.Test.Runtime.TestFixture$1(Bridge.ClientTest.Batch3.BridgeIssues.Bridge3253).BeforeTest(false, assert, Bridge.Test.Runtime.BridgeClientTestBatch3Runner.Bridge3253, void 0, ($t = new Bridge.Test.Runtime.TestContext(), $t.Method = "TestCanSetForReadonlyProperty()", $t.Line = "24", $t));
+                    var t = Bridge.Test.Runtime.TestFixture$1(Bridge.ClientTest.Batch3.BridgeIssues.Bridge3253).BeforeTest(false, assert, Bridge.Test.Runtime.BridgeClientTestBatch3Runner.Bridge3253, void 0, ($t = new Bridge.Test.Runtime.TestContext(), $t.Method = "TestCanSetForReadonlyProperty()", $t.Line = "36", $t));
                     Bridge.ClientTest.Batch3.BridgeIssues.Bridge3253.TestCanSetForReadonlyProperty();
                 }
             }
