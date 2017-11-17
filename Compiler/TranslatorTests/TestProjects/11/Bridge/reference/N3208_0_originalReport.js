e966c33f473abd8b4683289e14e0d365a6bf600b
Fabrício Zimmerer Murta <fabricio@object.net>
2017-11-16 22:43:27 -0200

    Updates reference files for NUnit project 11.

--- Compiler/TranslatorTests/TestProjects/11/Bridge/reference/N3208_0_originalReport.js
+++ Compiler/TranslatorTests/TestProjects/11/Bridge/reference/N3208_0_originalReport.js
@@ -3,6 +3,17 @@ Bridge.assembly("TestProject", function ($asm, globals) {
 
     define("N3208_originalReport_module", function () {
         var N3208_originalReport_module = { };
+        /** @namespace N3208_0_originalReport */
+
+        /**
+         * This differs from the original report on the issue because this does
+         not use an UMD module output (which is used in the next test case for
+         this same issue. The only difference applied here is, the module name
+         will be output between double quotes.
+         *
+         * @public
+         * @class N3208_originalReport_module.N3208_0_originalReport.Program
+         */
         Bridge.define("N3208_0_originalReport.Program", {
             $scope: N3208_originalReport_module,
             main: function Main () {
