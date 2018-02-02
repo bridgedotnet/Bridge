e0ca37cb9897c6d1ca7bd895f080cc1da731a8f2
Fabrício Zimmerer Murta <fabricio@object.net>
2018-02-02 15:45:49 -0200

    Expands and comments over unit tests for issues 2872 and 3390.

--- Tests/Batch3/BridgeIssues/3300/N3390.cs
+++ Tests/Batch3/BridgeIssues/3300/N3390.cs
@@ -10,7 +10,16 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
         public static void TestTernaryAssigmnment()
         {
             string msg = (true) ? msg = "left" : msg = "right";
-            Assert.AreEqual("left", msg);
+            Assert.AreEqual("left", msg, "true condition of inline if binds as expected.");
+
+            string msg2 = (false) ? msg2 = "left" : msg2 = "right";
+            Assert.AreEqual("right", msg2, "false condition of inline if binds as expected.");
+
+            string msg3 = (false) ? msg3 = "left" : ((false) ? msg3 = "middle" : msg3 = "right");
+            Assert.AreEqual("right", msg3, "false condition on chained inline if binds as expected.");
+
+            string msg4 = (false) ? msg4 = "left" : ((true) ? msg4 = "middle" : msg4 = "right");
+            Assert.AreEqual("middle", msg4, "true condition of chained inline if binds as expected.");
         }
     }
 }
