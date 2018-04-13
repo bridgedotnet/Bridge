209f992e28dbc4f4fed3e3d93871df97d10a9129
Fabrício Zimmerer Murta <fabricio@object.net>
2018-04-13 19:10:34 -0300

    Reviews NUnit tests.

--- Compiler/TranslatorTests/TestProjects/26/Bridge/reference/TestProject.meta.js
+++ Compiler/TranslatorTests/TestProjects/26/Bridge/reference/TestProject.meta.js
@@ -3,6 +3,6 @@ Bridge.assembly("TestProject", function ($asm, globals) {
 
 
     var $m = Bridge.setMetadata,
-        $n = [TestProject1];
-    $m($n[0].TestClass, function () { return {"att":1048577,"a":2,"m":[{"a":2,"isSynthetic":true,"n":".ctor","t":1,"sn":"ctor"}]}; });
+        $n = [];
+    $m("TestProject1.TestClass", function () { return {"att":1048577,"a":2,"m":[{"a":2,"isSynthetic":true,"n":".ctor","t":1,"sn":"ctor"}]}; });
 });
