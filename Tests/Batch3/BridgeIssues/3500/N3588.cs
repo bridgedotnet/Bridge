f3dabbb8ccd36593f60386109ed223deb7bcb9dd
Fabrício Zimmerer Murta <fabricio@object.net>
2018-05-23 22:36:30 -0300

    Adds comments to unit tests for issue 3588.

--- Tests/Batch3/BridgeIssues/3500/N3588.cs
+++ Tests/Batch3/BridgeIssues/3500/N3588.cs
@@ -5,6 +5,11 @@ using System.Linq;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// The tests here consists in ensuring the template substitution accepts
+    /// the {0} placeholder in some situations elucidated by the issue's
+    /// report.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3588 - {0}")]
     public class Bridge3588
     {
@@ -41,15 +46,16 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
         {
             public Vector3 F(Vector3 v)
             {
+                // This is how the code should be output to Bridge in JavaScript.
                 /*@
-             var Bridge3588 = {
-                Vec3: function (x, y, z) {
-                    this.x = x;
-                    this.y = y;
-                    this.z = z;
-                }
-             };
-             */
+                var Bridge3588 = {
+                    Vec3: function (x, y, z) {
+                        this.x = x;
+                        this.y = y;
+                        this.z = z;
+                    }
+                };
+                */
 
                 return new Vector3(v.x + 1, v.y + 2, v.z + 3);
             }
@@ -58,8 +64,9 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
         [Test]
         public static void TestOperatorTemplate()
         {
+            // This is how the code should be output to Bridge in JavaScript.
             /*@
-             var Bridge3588 = {
+            var Bridge3588 = {
                 Vec2: function (x, y) {
                     this.x = x;
                     this.y = y;
@@ -70,16 +77,16 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
                     this.y = y;
                     this.z = z;
                 }
-             };
-             */
+            };
+            */
 
             var u = new Utility();
             Vector2 v0 = new Vector2(1, 1);
             Vector2 v1 = u.F(v0);
 
-            Assert.True(((object)v1) is Vector2);
-            Assert.AreEqual(2, v1.x);
-            Assert.AreEqual(3, v1.y);
+            Assert.True(((object)v1) is Vector2, "Object cast does not lose ancestor type information.");
+            Assert.AreEqual(2, v1.x, "Template parameter substitution works as expected for a two-parameter expression.");
+            Assert.AreEqual(3, v1.y, "Template parameter substitution works as expected for a three-parameter expression.");
         }
     }
 }
