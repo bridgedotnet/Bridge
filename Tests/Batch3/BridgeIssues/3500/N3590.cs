9d6fa47aeb5e76718230f050bc93b2e8f0e7bedb
Fabrício Zimmerer Murta <fabricio@object.net>
2018-05-25 01:46:30 -0300

    Adds comments to unit tests for issue 3590.

--- Tests/Batch3/BridgeIssues/3500/N3590.cs
+++ Tests/Batch3/BridgeIssues/3500/N3590.cs
@@ -1,13 +1,16 @@
 using Bridge.Test.NUnit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [TestFixture(TestNameFormat = "#3590 - {0}")]
     public class Bridge3590
     {
+        /// <summary>
+        /// The class exploring the issue.
+        /// </summary>
         [External]
         [Name("Test")]
         public class Test
@@ -19,10 +22,19 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             public extern void Foo(params int[] arr);
         }
 
+        /// <summary>
+        /// Test by just making an instance of the Test class and populating it
+        /// with values.
+        /// </summary>
         [Test]
         public static void TestExternalExpandParams()
         {
+            // There will be a client-side "side effect" to change the value of
+            // this variable.
             var count = -1;
+
+            // The block below will output the client-side implementation of the
+            // "external" Test class.
             /*@
             var Test = function () {
                 count = arguments.length;
@@ -31,14 +43,15 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
                 };
             };
             */
+
             var arr = new[] { 1, 2, 3 };
 
             var test = new Test(arr);
-            Assert.AreEqual(3, count);
+            Assert.AreEqual(3, count, "External implementation of class can be run.");
 
             arr = new[] { 1, 2, 3, 4, 5 };
             test.Foo(arr);
-            Assert.AreEqual(5, count);
+            Assert.AreEqual(5, count, "External implementation of class' side effect works.");
         }
     }
 }
