5ccb5246ff6526feb7755958bd686f29344b249b
Fabrício Zimmerer Murta <fabricio@object.net>
2018-06-21 17:50:18 -0300

    Adds comments to unit tests for issue 3613.

--- Tests/Batch3/BridgeIssues/3600/N3613.cs
+++ Tests/Batch3/BridgeIssues/3600/N3613.cs
@@ -6,9 +6,18 @@ using System.Linq;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// The test here ensures that invalid cast exception is thrown when a type
+    /// is boxed then cast back, when the type can't be actually cast over,
+    /// following native .NET implementation.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3613 - {0}")]
     public class Bridge3613
     {
+        /// <summary>
+        /// Makes the test boxing a byte variable then trying to cast it back
+        /// as an integer.
+        /// </summary>
         [Test]
         public static void TestUnboxCast()
         {
@@ -16,7 +25,7 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
                 byte byteVal = 255;
                 object boxed = byteVal;
                 int unboxed = (int)boxed;
-            });
+            }, "Boxing byte and trying to cast back to int throws InvalidCastException.");
         }
     }
 }
