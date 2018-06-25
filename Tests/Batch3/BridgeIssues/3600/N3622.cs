4dbd85bba01e766af7fa55b7af8f74fc1e2434e7
Fabrício Zimmerer Murta <fabricio@object.net>
2018-06-25 14:01:54 -0300

    Adds comments to 3622's unit tests.

--- Tests/Batch3/BridgeIssues/3600/N3622.cs
+++ Tests/Batch3/BridgeIssues/3600/N3622.cs
@@ -6,9 +6,16 @@ using System.Linq;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// Ensuring base class calling works even when inheriting from an
+    /// external class.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3622 - {0}")]
     public class Bridge3622
     {
+        /// <summary>
+        /// A custom implementation of a class.
+        /// </summary>
         [Init(InitPosition.Top)]
         public static void Init()
         {
@@ -22,12 +29,18 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             */
         }
 
+        /// <summary>
+        /// A class mapping to the custom implemented class.
+        /// </summary>
         [Virtual]
         [Name("Bridge3622_A")]
         public class A
         {
         }
 
+        /// <summary>
+        /// A class inheriting from the mapped C# class.
+        /// </summary>
         public class B : A
         {
             public B()
@@ -35,11 +48,15 @@ namespace Bridge.ClientTest.Batch3.BridgeIssues
             }
         }
 
+        /// <summary>
+        /// Tests by instantiating the child class and checking whether the
+        /// arbitrarily-crafted external class constructor was called.
+        /// </summary>
         [Test]
         public static void TestExternalBaseDefaultCtor()
         {
             var b = new B();
-            Assert.AreEqual(true, b["A_initialized"]);
+            Assert.AreEqual(true, b["A_initialized"], "External, inherited, and mapped class' constructor called.");
         }
     }
 }
