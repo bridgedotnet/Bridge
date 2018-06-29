7f8c62ba2e2c9de43b8a55d2510e3bcb6c497d0f
Fabrício Zimmerer Murta <fabricio@object.net>
2018-06-29 19:20:15 -0300

    Refreshes output files with 3477 unit tests' changes.

--- Tests/Runner/Batch3/Bridge.Test/bridge.clienttest.batch3.runner.js
+++ Tests/Runner/Batch3/Bridge.Test/bridge.clienttest.batch3.runner.js
@@ -16329,7 +16329,7 @@ Bridge.assembly("Bridge.Test.Bridge.ClientTest.Batch3", function ($asm, globals)
             methods: {
                 TestOrderByComparer: function (assert) {
                     var $t;
-                    var t = Bridge.Test.Runtime.TestFixture$1(Bridge.ClientTest.Batch3.BridgeIssues.Bridge3477).BeforeTest(false, assert, Bridge.Test.Runtime.BridgeClientTestBatch3Runner.Bridge3477, void 0, ($t = new Bridge.Test.Runtime.TestContext(), $t.Method = "TestOrderByComparer()", $t.Line = "15", $t));
+                    var t = Bridge.Test.Runtime.TestFixture$1(Bridge.ClientTest.Batch3.BridgeIssues.Bridge3477).BeforeTest(false, assert, Bridge.Test.Runtime.BridgeClientTestBatch3Runner.Bridge3477, void 0, ($t = new Bridge.Test.Runtime.TestContext(), $t.Method = "TestOrderByComparer()", $t.Line = "20", $t));
                     Bridge.ClientTest.Batch3.BridgeIssues.Bridge3477.TestOrderByComparer();
                 }
             }
