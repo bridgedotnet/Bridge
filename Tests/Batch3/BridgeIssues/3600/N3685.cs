41ba20f1167142b64ca1d8e3bccd1f5a67b0be5d
Fabrício Zimmerer Murta <fabricio@object.net>
2018-08-21 15:46:50 -0300

    Expands and comments on unit tests for issue 3685.

--- Tests/Batch3/BridgeIssues/3600/N3685.cs
+++ Tests/Batch3/BridgeIssues/3600/N3685.cs
@@ -1,23 +1,32 @@
-using Bridge.Html5;
 using Bridge.Test.NUnit;
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Bridge.ClientTest.Batch3.BridgeIssues
 {
+    /// <summary>
+    /// THis tests whether the System.IList.IsFixed system property returns the
+    /// correct result depending on the list being fixed-size or not.
+    /// </summary>
     [TestFixture(TestNameFormat = "#3685 - {0}")]
     public class Bridge3685
     {
+        /// <summary>
+        /// Simply tests a fixed array and a List against its fixed size
+        /// property result.
+        /// </summary>
         [Test]
         public static void TestIsFixedSize()
         {
             IList arr = new int[10];
             IList list = new List<int>();
+            IList ltdList = new List<int>(10);
 
-            Assert.True(arr.IsFixedSize);
-            Assert.False(list.IsFixedSize);
+            Assert.True(arr.IsFixedSize, "IList.IsFixedSize is true for a fixed-length array.");
+            Assert.False(list.IsFixedSize, "IList.IsFixedSize is false for an instance of System.Collections.Generic.List.");
+
+            // Right or wrong, this matches native .NET results.
+            Assert.False(ltdList.IsFixedSize, "IList.IsFixedSize is false for an instance of System.Collections.Generic.List with specified capacity.");
         }
     }
 }
